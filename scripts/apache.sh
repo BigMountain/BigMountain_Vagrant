0390786e334998de77f61d0e9a4faeb2b07106e1
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2014-05-14 19:06:22 -0500

    ADD VAGRANT00001 - Servidor Web: Apache
    Adición de Script para Servidor Web Apache

---
+++ scripts/apache.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing Apache Server"
+
+# Add repo for lastest FULL stable Apache
+sudo add-apt-repository -y ppa:ondrej/apache2
+
+# Update again
+sudo apt-key update
+sudo apt-get update
+
+sudo apt-get install -y --force-yes apache2
+
