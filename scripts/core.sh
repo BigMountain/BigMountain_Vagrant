bc718e604d8eb63c1c1d4cad23c5ff06dd4fd133
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2014-05-15 07:59:54 -0500

    UPDATE VAGRANT00002 - Instalación de Certificado
    Agregación de Certificado SSL

--- scripts/core.sh
+++ scripts/core.sh
@@ -7,3 +7,25 @@ sudo apt-get update
 
 # Install base packages
 sudo apt-get install -y curl unzip git-core ack-grep python-software-properties
+
+echo ">>> Installing *.xip.io self-signed SSL"
+
+SSL_DIR="/etc/ssl/xip.io"
+DOMAIN="*.xip.io"
+PASSPHRASE="BigMountain_Vagrant"
+
+SUBJ="
+C=US
+ST=Connecticut
+O=BigMountain_Vagrant
+localityName=New Haven
+commonName=$DOMAIN
+organizationalUnitName=
+emailAddress=
+"
+
+sudo mkdir -p "$SSL_DIR"
+
+sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
+sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
+sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
