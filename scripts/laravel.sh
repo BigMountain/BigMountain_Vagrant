40ef3954f4ae2df3957c29e8a3180bbd799748b6
José Ignacio Jaraba Atehortúa <jijaraba@hotmail.com>
2014-05-16 15:45:22 -0500

    FIX VAGRANT00009 - Corrección nombre de Archivo
    Laravel.phar

--- scripts/laravel.sh
+++ scripts/laravel.sh
@@ -3,5 +3,5 @@
 echo ">>> Installing Laravel"
 
 wget http://www.laravel.com/laravel.phar
-sudo mv laravel /usr/local/bin/laravel
+sudo mv laravel.phar /usr/local/bin/laravel
 chmod +x /usr/local/bin/laravel
